bc00db586d83b0bb350dae5fb3a2822b4861486a
Anuj <anizrana@gmail.com>
2023-03-19 11:26:34 +0545

    Setup project

---
+++ src/routers/Routers.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Signup from  '../pages/Signup'
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route>
+        <Route path='/' element={<Home/> }/>
+        <Route path='/login' element={<Login/> }/>
+        <Route path='/signup' element={<Signup/> }/>
+        <Route path='/' element={<Home/> }/>
+        </Route>
+    </Routes>
+  )
+}
+
+export default Routers
